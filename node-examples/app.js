4534c400bfd81134aa6c8779b9c70183055fac14
Makayla Paasch <kaydarae19@gmail.com>
2022-08-29 10:55:15 -0500

    Express Generator

--- node-examples/app.js
+++ node-examples/app.js
@@ -1,24 +1,56 @@
-const rect = require("./rectangle");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-function solveRect(l, w) {
-  console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+var indexRouter = require("./routes/index");
 
-  rect(l, w, (err, rectangle) => {
-    if (err) {
-      console.log("ERROR:", err.message);
-    } else {
-      console.log(
-        `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
-      );
-      console.log(
-        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
-      );
-    }
-  });
-  console.log("This statement is logged after the call to rect()");
-}
+var usersRouter = require("./routes/users");
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(5, -3);
+const campsiteRouter = require("./routes/campsiteRouter");
+
+const promotionRouter = require("./routes/promotionRouter");
+
+const partnerRouter = require("./routes/partnerRouter");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+app.use("/users", usersRouter);
+
+app.use("/campsites", campsiteRouter);
+
+app.use("/promotions", promotionRouter);
+
+app.use("/partners", partnerRouter);
+
+// catch 404 and forward to error handler
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
